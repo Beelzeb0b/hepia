d5a75864d8b95621f39c3bda58678facef80d6de
Unknown <ventur213@gmail.com>
2018-02-21 19:14:27 +0100

    C tp1 secret number v2

--- Algorithmique/C/TP1_NombreSecret/secret_number.c
+++ Algorithmique/C/TP1_NombreSecret/secret_number.c
@@ -2,25 +2,33 @@
 #include <stdlib.h>
 #include <time.h>
 int number, max, input, iteration = 0;
+
+int calcul_optimized_iteration(int number, int iteration){
+	return number + iteration;
+}
+
+int rand_v2(int a, int b){
+   return rand()%(b-a) +a;
+}
+
+}
 int main(){
 	printf("Enter max value :");
 	scanf("%d", &max);
-	//// select secret number
+	srand(time(NULL)); // init random
+	number = rand_v2(0, max);
 	printf("Secret number, Start!\n");
-	while (entre != number){
+	do {
 		printf("Enter your number :");
-		scanf("%d", &input)
-		if(input > number){
+		scanf("%d", &input);
+		if(input > number)
 			printf("Too big!\n");
-		else
+		else if (input < number)
 			printf("Too small!\n");
 		iteration++;
-	}
+	} while (input != number);
 	printf("Well done, you completed the game with %d attempt(s)", iteration);
-	int opti_iter = calcul_optimized_iteration(number);
+	int opti_iter = calcul_optimized_iteration(number, 0);
 	printf("You can found the number with only %d attempt(s)",opti_iter);
 }	
 
-int calcul_optimized_iteration(number, iteration = 0){
-	
-}
